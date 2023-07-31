b88a5090eebfedbf1d25249b29e68f7bb4813b8a
RowTheBoat <rowenrlink@gmail.com>
2023-07-31 15:29:50 -0400

    Initial Commit

---
+++ Hopper/HopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopperApp.swift
+//  Hopper
+//
+//  Created by Rowen Link on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct HopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
