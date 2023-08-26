6c0be29ae2391e940617b6c3c3d4b71774521412
Arupava Saha <arupava.saha@gmail.com>
2023-08-26 16:09:59 -0400

    Created a smoother transition between different previews when selected

--- Hopper/MapScreenView.swift
+++ Hopper/MapScreenView.swift
@@ -15,7 +15,7 @@ struct MapScreenView: View {
     
     @StateObject private var locationManager = LocationManager()
     @ObservedObject private var viewModel = barViewModel()
-    @State private var preview = BarPreviewView(bar: Bar(id : "", numUsers: 0, name: "none", image:"", longitude: "0", latitude: "38.034833", emoji: "🫣", coordinates: CLLocationCoordinate2D(latitude: Double( "38.034833") ?? 0.0, longitude: Double("-78.500381") ?? 0.0), waitTime: 2))
+//    @State private var preview = BarPreviewView(bar: Bar(id : "", numUsers: 0, name: "none", image:"", longitude: "0", latitude: "38.034833", emoji: "🫣", coordinates: CLLocationCoordinate2D(latitude: Double( "38.034833") ?? 0.0, longitude: Double("-78.500381") ?? 0.0), waitTime: 2))
     @State private var region = MKCoordinateRegion.cVilleRegion().getBinding()
     
     
@@ -26,7 +26,12 @@ struct MapScreenView: View {
                 header
                 locationButton
                 Spacer()
-                preview
+                if selectedBar != nil {
+                    BarPreviewView(bar: selectedBar!)
+                        .transition(.moveAndFade)
+                        .animation(.easeInOut, value: selectedBar)
+                }
+                  
             }
             
         }
@@ -51,7 +56,7 @@ extension MapScreenView{
                             selectedBar = nil
                         } else {
                             selectedBar = bar
-                            preview = BarPreviewView(bar: bar)
+//                            preview = BarPreviewView(bar: bar)
                             region = MKCoordinateRegion(center: bar.coordinates, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)).getBinding()
                         }
                             
@@ -98,3 +103,10 @@ extension MapScreenView{
     }
 }
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity))
+    }
+}
